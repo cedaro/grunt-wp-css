ed14a04488bf2d6cf358fe2ffbd76b815a80c392
Brady Vercher <brady@blazersix.com>
2014-05-13 08:42:20 -0700

    Add an option to specify a bundled config file.

--- tasks/wpcss.js
+++ tasks/wpcss.js
@@ -12,37 +12,42 @@ module.exports = function( grunt ) {
 
 	var _ = require( 'underscore' ),
 		Comb = require( 'csscomb' ),
-		cssbeautify = require( 'cssbeautify' );
+		cssbeautify = require( 'cssbeautify' ),
+		fs = require( 'fs' ),
+		path = require( 'path' );
 
-	/**
-	 * Format CSS according to the rules defined in .csscomb.json.
-	 *
-	 * Rules are sorted alphabetically by default, with the following exceptions:
-	 * - Position units (top, right, bottom, left) follow the 'position' declaration.
-	 */
 	grunt.registerMultiTask( 'wpcss', function() {
 		var comb = new Comb(),
-			config = require( './config/wp.json' ),
-			configKeys = _.keys( config ),
+			config = {},
+			configKeys = {},
+			configPath = {},
 			userConfig = {},
 			options;
 
-		options = this.options();
+		options = this.options({
+			config: 'default'
+		});
+
+		configPath = path.resolve( __dirname, 'config/' + options.config + '.json' );
+		if ( fs.existsSync( configPath ) ) {
+			config = require( configPath );
+		} else {
+			grunt.fatal( 'Invalid config defined.' );
+		}
 
 		if ( grunt.file.exists( '.csscomb.json' ) ) {
 			userConfig = grunt.file.readJSON( '.csscomb.json' );
 		}
 
+		configKeys = _.keys( config );
 		config = _.extend( config, userConfig, options );
 		config = _.pick( config, configKeys );
 
 		comb.configure( config );
 
 		this.files.forEach(function( f ) {
-			var contents, combed;
-
 			// Read and combine source files into the 'contents' var.
-			contents = f.src.filter(function( filepath ) {
+			var contents = f.src.filter(function( filepath ) {
 				// Warn on and remove invalid source files (if nonull was set).
 				if ( ! grunt.file.exists( filepath ) ) {
 					grunt.log.warn( 'Source file "' + filepath + '" not found.' );
@@ -55,8 +60,9 @@ module.exports = function( grunt ) {
 			}).join( '\n\n' );
 
 			contents = cssbeautify( contents );
-			combed = comb.processString( contents );
-			grunt.file.write( f.dest, combed );
+			contents = comb.processString( contents );
+
+			grunt.file.write( f.dest, contents );
 		});
 	});
 
