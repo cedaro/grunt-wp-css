29266a8fa975c7c86fdee89c5e9384cd3f836b6e
Brady Vercher <brady@blazersix.com>
2014-08-03 15:53:55 -0700

    Update comment spacing regex to pass the unit test.

--- tasks/wpcss.js
+++ tasks/wpcss.js
@@ -62,6 +62,9 @@ module.exports = function( grunt ) {
 			contents = cssbeautify( contents );
 			contents = comb.processString( contents );
 
+			// Add two newlines before all comments that trail a closing curly brace.
+			contents = contents.replace( /}\s+\/\*/, '}\n\n/*' );
+
 			// Collapse extra newlines.
 			contents = contents.replace( /\n{3,}/g, '\n\n' );
 
